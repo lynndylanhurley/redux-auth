400501e892c1ce9eb9be266ee5e47636aff7d6dc
Lynn Hurley <lynn.dylan.hurley@gmail.com>
2015-12-09 00:43:09 -0600

    update dependencies, generate release

--- webpack.release.js
+++ webpack.release.js
@@ -13,12 +13,38 @@ module.exports = {
     chunkFilename: "[name].[id].js",
     publicPath:    "dist/"
   },
+  externals: {
+    "react": "react",
+    "classnames": "classnames",
+    "js-cookie": "js-cookie",
+    "cookie": "cookie",
+    "extend": "extend",
+    "history": "history",
+    "immutable": "immutable",
+    "isomorphic-fetch": "isomorphic-fetch",
+    "node-fetch": "node-fetch",
+    "query-string": "query-string",
+    "querystring": "querystring",
+    "react-bootstrap": "react-bootstrap",
+    "react-dom": "react-dom",
+    "react-modal": "react-modal",
+    "react-redux": "react-redux",
+    "redux": "redux",
+    "redux-immutablejs": "redux-immutablejs",
+    "react-router": "react-router",
+    "redux-router": "redux-router",
+    "redux-thunk": "redux-thunk",
+    "thunk": "thunk",
+    "rc-dialog": "rc-dialog",
+    "react-loader": "react-loader",
+    "url": "url"
+  },
   plugins: [
     new webpack.DefinePlugin({__CLIENT__: true, __SERVER__: false}),
     new webpack.DefinePlugin({"process.env": {NODE_ENV: "\"production\""}}),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.OccurenceOrderPlugin()
+    //new webpack.optimize.UglifyJsPlugin()
   ],
   module:  {
     loaders: [
